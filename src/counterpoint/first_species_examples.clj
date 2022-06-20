6abf692a0a60de02e1be15c0a3cbc133f6baecc6
Dimitris Dranidis <dranidis@gmail.com>
2022-06-20 12:47:47 +0300

    Solve circular dep

---
+++ src/counterpoint/first_species_examples.clj
@@ -0,0 +1,53 @@
+(ns counterpoint.first-species-examples 
+  (:require [clojure.java.shell :as sh]
+            [counterpoint.first-species :refer [first-species-rules?]]
+            [counterpoint.first-species-type :refer [make-first-species]]
+            [counterpoint.lilypond :refer [first-species->lily]]
+            [counterpoint.melody :refer [make-melody melodic-intervals]]
+            [counterpoint.notes :as n]))
+
+(comment
+  (def species (let [counterpoint-melody
+                     (make-melody n/d3 n/d3 n/a4 n/f3 n/e3 n/d3 n/c3 n/e3 n/d3 n/c#3 n/d3)
+                     cantus-firmus
+                     (make-melody n/d4 n/f4 n/e4 n/d4 n/g4 n/f4 n/a5 n/g4 n/f4 n/e4 n/d4)]
+                 (make-first-species cantus-firmus counterpoint-melody :below)))
+
+  (def species (let [counterpoint-melody
+                     (make-melody n/a4 n/a4 n/g3 n/a4 n/b4 n/c4 n/c4 n/b4 n/d4 n/c#4 n/d4)
+                     cantus-firmus
+                     (make-melody n/d3 n/f3 n/e3 n/d3 n/g3 n/f3 n/a4 n/g3 n/f3 n/e3 n/d3)]
+                 (make-first-species cantus-firmus counterpoint-melody :above)))
+
+  (def species
+    (let [counterpoint-melody
+          (make-melody n/e4 n/f4 n/g4 n/a5 n/e4 n/f4 n/f4 n/d4 n/e4)
+          _ (print (melodic-intervals counterpoint-melody))
+          cantus-firmus
+          (make-melody n/e3 n/d3 n/e3 n/f3 n/g3 n/a4 n/d3 n/f3 n/e3)]
+      (make-first-species cantus-firmus counterpoint-melody :above)))
+
+
+
+  (def species (let [counterpoint-melody
+                     (make-melody n/d4 n/c4 n/d4 n/f4 n/e4 n/a5 n/g4 n/e4 n/d4 n/c#4 n/d4)
+                     cantus-firmus ;; haydn
+                     (make-melody n/d3 n/e3 n/f3 n/d3 n/a4 n/f3 n/e3 n/g3 n/f3 n/e3 n/d3)]
+                 (make-first-species cantus-firmus counterpoint-melody :above)))
+
+  (def species (let [counterpoint-melody
+                     (make-melody n/c4 n/a4 n/b4 n/c4 n/e4 n/d4 n/g3 n/b4 n/c4)
+                     cantus-firmus ;; salieri
+                     (make-melody n/c3 n/f3 n/e3 n/a4 n/g3 n/f3 n/e3 n/d3 n/c3)]
+                 (make-first-species cantus-firmus counterpoint-melody :above)))
+  (first-species-rules? species)
+  (first-species->lily species)
+
+
+  (sh/sh "timidity" "resources/temp.midi")
+
+  (print (first-species->lily species))
+
+  ;
+  )
+  
--- test/counterpoint/intervals_test.clj
+++ test/counterpoint/intervals_test.clj
@@ -1,18 +1,21 @@
 (ns counterpoint.intervals-test
   (:require [clojure.test :refer [deftest is testing]]
             [counterpoint.intervals :refer [note-at-diatonic-interval m3 m3-]]
-            [counterpoint.notes :as notes]))
+            [counterpoint.notes :as n]))
 
 (deftest get-note-at-interval-test
   (testing "key c: a 3"
-    (is (= (notes/make-note-nooctave :c :natural) (note-at-diatonic-interval :c :a 3))))
+    (is (= (n/make-note-nooctave :c :natural) (note-at-diatonic-interval :c :a 3))))
 
   (testing "key c: a -3"
-    (is (= (notes/make-note-nooctave :f :natural) (note-at-diatonic-interval :c :a -3))))
+    (is (= (n/make-note-nooctave :f :natural) (note-at-diatonic-interval :c :a -3))))
 
   (testing "key d: f# 5"
-    (is (= (notes/make-note-nooctave :c :sharp) (note-at-diatonic-interval :d :f 5))))
+    (is (= (n/make-note-nooctave :c :sharp) (note-at-diatonic-interval :d :f 5))))
 
   (testing "key db: a 1 3 5 6"
-    (is (= '([:a :flat] [:c :natural] [:e :flat] [:f :natural])
+    (is (= [(n/make-note-nooctave :a :flat)
+            (n/make-note-nooctave :c :natural)
+            (n/make-note-nooctave :e :flat)
+            (n/make-note-nooctave :f :natural)]
            (map #(note-at-diatonic-interval :db :a %) [1 3 5 6])))))
