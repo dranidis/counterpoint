96e61fc6311891420c9be6f52324f5eafad234d4
Dimitris Dranidis <dranidis@gmail.com>
2022-07-04 19:16:19 +0300

    Add test for generator

---
+++ test/counterpoint/generate_first_species_test.clj
@@ -0,0 +1,17 @@
+(ns counterpoint.generate-first-species-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [counterpoint.cantus-firmi-examples :refer [fux-d]]
+            [counterpoint.first-species :refer [first-species-rules?]]
+            [counterpoint.first-species-type :refer [make-first-species]]
+            [counterpoint.generate-first-species :refer [generate-reverse-random-counterpoint]]))
+
+(deftest maximum-range-M10
+  (testing "above"
+    (let [cp (generate-reverse-random-counterpoint :above :c fux-d)
+          species (make-first-species fux-d cp :above)]
+      (is (first-species-rules? species))))
+  
+  (testing "below"
+    (let [cp (generate-reverse-random-counterpoint :below :c fux-d)
+          species (make-first-species fux-d cp :below)]
+      (is (first-species-rules? species)))))
